bdb8fe956394533a356e90bcc6f614647c00d7ee
xlcrr <info@openlittermap.com>
2022-07-05 20:55:24 +0100

    fix migration

--- database/migrations/2022_03_12_111143_create_buildings_data_table.php
+++ database/migrations/2022_03_12_111143_create_buildings_data_table.php
@@ -13,19 +13,19 @@ class CreateBuildingsDataTable extends Migration
      */
     public function up()
     {
-//        Schema::create('buildings_data', function (Blueprint $table) {
-//            $table->id();
-//            $table->unsignedInteger('user_id');
-//            $table->foreign('user_id')->references('id')->on('users');
-//            $table->unsignedBigInteger('osm_id');
-//            $table->foreign('osm_id')->references('osm_id')->on('buildings');
-//            $table->integer('verified')->default(0);
-//            $table->integer('year')->nullable();
-//            $table->string('key')->nullable();
-//            $table->string('value')->nullable();
-//            $table->text('filepath')->nullable();
-//            $table->timestamps();
-//        });
+        Schema::create('buildings_data', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('osm_id');
+            $table->foreign('osm_id')->references('osm_id')->on('buildings');
+            $table->integer('verified')->default(0);
+            $table->integer('year')->nullable();
+            $table->string('key')->nullable();
+            $table->string('value')->nullable();
+            $table->text('filepath')->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
